ead3d461dc291846acb99ce7af3ee65ad4016c84
Leona Raine <149066635+lryona@users.noreply.github.com>
2024-09-01 02:21:59 -0700

    Update Basketball_application.java

--- Basketball_application.java
+++ Basketball_application.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.personal_projects;
+package com.mycompany.small_projects;
 
 import java.util.Scanner;
 
