bbd21cb6ae8571e522cb9b03147ccfdd8083915a
Leona Raine <149066635+lryona@users.noreply.github.com>
2024-09-01 21:04:46 -0700

    Basketball_application.java

--- Basketball_application.java
+++ Basketball_application.java
@@ -1,15 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
-package com.mycompany.small_projects;
+package basketball_application;
 
 import java.util.Scanner;
 
 public class Basketball_application {
     
     public static void main(String[] args) {
-    
+        
         Scanner scanner = new Scanner(System.in);
         
         int choice;
@@ -20,7 +16,7 @@ public class Basketball_application {
         String name;
         int age = 0;
         int applicant_count = 0;
-        
+    
         while(true) {
             System.out.println("Menu");
             System.out.println("-----------------");
@@ -28,61 +24,60 @@ public class Basketball_application {
             System.out.println("[2] Apply for the team");
             System.out.println("[3] Exit");
             System.out.println("-----------------");
-            System.out.println("Choice:");
-        
+            System.out.println("Pick a choice:");
+            
             choice = scanner.nextInt();
             
             scanner.nextLine();
-        
+            
             switch(choice) {
-            case 1:
-                
-                if(open_slots != 0 && min_age !=0 && max_age != 0) {
-                    System.out.println("You are not allowed to change requirements.");
-                } else {
-                    System.out.println("Enter number of open slot/s:");
-                    open_slots = scanner.nextInt();
-                    applicants = new String[open_slots];
-                
-                    System.out.println("Enter minimum age:");
-                    min_age = scanner.nextInt();
-                    System.out.println("Enter maximum age:");
-                    max_age = scanner.nextInt();
-                
-                    scanner.nextLine();
-                }
-            break;
-            case 2:
-                System.out.println("Enter name:");
-                name = scanner.nextLine();
-                System.out.println("Enter age:");
-                age = scanner.nextInt();
-                
-                if(applicant_count < open_slots) {
-                    if(age >= min_age && age <= max_age) {
-                        System.out.println("You are now applied for the team!");
-                        applicants[applicant_count] = name;
-                        applicant_count++;
+                case 1: 
+                    if(open_slots != 0 && min_age != 0 && max_age != 0) {
+                        System.out.println("You are not allowed to change requirement. Exit the system to restart.");
                     } else {
-                        System.out.println("You are not applicable for the team.");
+                        System.out.println("Enter number of open slot/s");
+                        open_slots = scanner.nextInt();
+                        applicants = new String[open_slots];
+                    
+                        System.out.println("Enter minimum age");
+                        min_age = scanner.nextInt();
+                        System.out.println("Enter maximum age");
+                        max_age = scanner.nextInt();
                     }
-                } else {
-                    System.out.println("There are no open slots left.");
-                }
-            break;
-            case 3:
-                if(applicant_count != 0) {
-                    for(int i=0;i<applicant_count;i++) {
-                    System.out.println("Successful applicants:");
-                    System.out.println(applicants[i]);
+                break;
+                case 2:
+                    
+                    if(open_slots != 0 && min_age != 0 && max_age != 0) {
+                        System.out.println("Enter the applicant's name:");
+                        name = scanner.nextLine();
+                        System.out.println("Enter the applicant's age:");
+                        age = scanner.nextInt();
+                    
+                        if(applicant_count < open_slots) {
+                            if(age >= min_age && age <= max_age) {
+                                System.out.println("You are now applied for the team!");
+                                applicants[applicant_count] = name;
+                                applicant_count++;
+                            } else {
+                                System.out.println("You are not applicable for the team.");
+                            }
+                        } else {
+                            System.out.println("There are no open slots available for the team.");
+                        }
+                    } else {
+                        System.out.println("Place the necessary requirements first.");
                     }
-                } else {
-                    System.out.println("There are no applicants.");
-                }
-                scanner.close();
-                System.exit(0);
-            break;
-            default: System.out.println("Invalid choice. Please try again.");
+                break;
+                case 3:
+                    for(int i = 0; i < applicant_count; i++) {
+                        System.out.println("Successful applicants:");
+                        System.out.println(applicants[i]);
+                    }
+                    
+                    scanner.close();
+                    System.exit(0);
+                break;
+                default: System.out.println("Invalid choice. Please try again.");
             }
         }
     }
